6ee6380c98ec1a788b41cee2ee121c75b91840fd
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 19:24:45 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,16 @@ variable "AWS_ACCESS_KEY"{
 }
 variable "AWS_SECRET_ACCESS_KEY"{ 
 }
+
+# AWS EC2 Instance Terraform Variables
+# EC2 Instance Variables
+# AWS EC2 Instance Type
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type = string 
+}
+# AWS EC2 Instance Key Pair
+variable "instance_keypair" {
+  description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
+  type = string
+}
