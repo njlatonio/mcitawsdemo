c939174b6175ccdb59d1ab8c36c861926b4469a7
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 19:42:41 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -83,3 +83,14 @@ output "private_sg_group_name" {
   value       = module.private_sg.security_group_name
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////
+# Launch Template Outputs
+output "launch_template_id" {
+  description = "Launch Template ID"
+  value = aws_launch_template.my_launch_template.id
+}
+
+output "launch_template_latest_version" {
+  description = "Launch Template Latest Version"
+  value = aws_launch_template.my_launch_template.latest_version
+}
