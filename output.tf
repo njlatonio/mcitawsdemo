9d18df40fe7cd45625506cdf128f2cba0e4b51bf
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 19:47:19 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -94,3 +94,19 @@ output "launch_template_latest_version" {
   description = "Launch Template Latest Version"
   value = aws_launch_template.my_launch_template.latest_version
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+# AWS EC2 Instance Terraform Outputs
+# Public EC2 Instances - Bastion Host
+
+## ec2_bastion_public_instance_ids
+output "ec2_bastion_public_instance_ids" {
+  description = "List of IDs of instances"
+  value       = module.ec2_public.id
+}
+
+## ec2_bastion_public_ip
+output "ec2_bastion_public_ip" {
+  description = "List of public IP addresses assigned to the instances"
+  value       = module.ec2_public.public_ip 
+}
