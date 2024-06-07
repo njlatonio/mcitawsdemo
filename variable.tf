999a58c6a27d84572235f4538cc125eae6127214
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 19:01:46 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+# Input Variables
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-east-1"  
+}
+variable "AWS_ACCESS_KEY"{
+  
+}
+variable "AWS_SECRET_ACCESS_KEY"{
+  
+}
