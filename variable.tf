36178ee29aa47deb0bf3a82be27981dda9ffaf0d
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 19:02:11 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,9 +5,9 @@ variable "aws_region" {
   type = string
   default = "us-east-1"  
 }
+
+# Access keys
 variable "AWS_ACCESS_KEY"{
-  
 }
-variable "AWS_SECRET_ACCESS_KEY"{
-  
+variable "AWS_SECRET_ACCESS_KEY"{ 
 }
