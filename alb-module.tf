05264fcdbcad128a60ab9a34e5543db1c09d34d5
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 20:15:58 -0400

    Create alb-module.tf

---
+++ alb-module.tf
@@ -0,0 +1,58 @@
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "myalb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+  security_groups = [module.loadbalancer_sg.security_group_id]
+  
+# For example only
+  enable_deletion_protection = false
+
+  client_keep_alive = 7200
+
+# Listeners
+  listeners = {
+    # Listener-1: TCP Listener
+    my-tcp = {
+      port     = 80
+      protocol = "TCP"
+      forward = {
+        target_group_key = "mytg1"
+      }
+    }# End Listener-1: TCP Listener
+    # Listener-2: TLS Listener (SSL)
+    my-tls = {
+      port            = 443
+      protocol        = "TLS"
+      certificate_arn = module.acm.acm_certificate_arn
+      forward = {
+        target_group_key = "mytg1"
+      }
+    }# End Listener-2: TLS Listener (SSL)
+  }# End Listeners Block
+
+# Target Groups
+  target_groups = { 
+    # Target Group-1: mytg1
+    mytg1 = {
+      create_attachment = false          
+      name_prefix          = "mytg1-"
+      protocol             = "TCP"
+      port                 = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+      }# End Health Check Block
+    }# End Target Group-1: mytg1
+  }
+
+  #tags = local.tags
+}
