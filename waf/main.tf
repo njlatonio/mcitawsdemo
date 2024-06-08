297b09e4807ebb2e13d5e86d948d6a2d00ab8ad3
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-07 20:58:23 -0400

    Update main.tf

--- waf/main.tf
+++ waf/main.tf
@@ -65,3 +65,8 @@ resource "aws_wafv2_web_acl" "example" {
     sampled_requests_enabled   = false
   }
 }
+
+resource "aws_wafv2_web_acl_association" "example" {
+  resource_arn = aws_api_gateway_stage.example.arn
+  web_acl_arn  = aws_wafv2_web_acl.example.arn
+}
