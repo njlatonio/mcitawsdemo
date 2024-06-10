1ddbec874df39368ccc873085467932a7423a7a7
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-10 17:16:57 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -97,3 +97,9 @@ variable "instance_keypair" {
   description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
   type = string
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+# AWS Web ACL variables
+variable "web_acl_name" {
+  type=string  
+}
