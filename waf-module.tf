8eb13ff8734760cc739a794afaae73d75ea1a019
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-10 18:16:15 -0400

    Update waf-module.tf

--- waf-module.tf
+++ waf-module.tf
@@ -1,5 +1,14 @@
+/*
 module "waf" {
   source = "./waf/"
+
+  web_acl_name = var.web_acl_name
+  #alb_arn = module.nlb.arn
+}
+*/
+
+module "waf" {
+  source = "./"
 
   web_acl_name = var.web_acl_name
   #alb_arn = module.nlb.arn
