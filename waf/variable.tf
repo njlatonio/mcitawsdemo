2b83c6a97957e71d70aa36cdca8b98521e91f9c9
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-06-10 21:03:37 -0400

    Update variable.tf

--- waf/variable.tf
+++ waf/variable.tf
@@ -1,16 +1,3 @@
-# Input Variables
-# AWS Region
-variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-}
-
-# Access keys
-variable "AWS_ACCESS_KEY"{
-}
-variable "AWS_SECRET_ACCESS_KEY"{ 
-}
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 # VPC Input Variables
 # VPC Name
@@ -82,20 +69,6 @@ variable "vpc_single_nat_gateway" {
   description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
   type = bool
   default = true
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-# AWS EC2 Instance Terraform Variables
-# EC2 Instance Variables
-# AWS EC2 Instance Type
-variable "instance_type" {
-  description = "EC2 Instance Type"
-  type = string 
-}
-# AWS EC2 Instance Key Pair
-variable "instance_keypair" {
-  description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
-  type = string
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
